4117af4d90c66b7337520f0b092edddb32a21021
Kiyle Winborne <kiyleawinborne@gmail.com>
2023-03-31 14:00:32 -0400

    Fixed Namespaces for IAircraft.cs
    Added placeholder methods for Aircraft.cs - Implements IAircraft.cs

--- AirEase-AMS/Application/Defs/IAircraft.cs
+++ AirEase-AMS/Application/Defs/IAircraft.cs
@@ -1,6 +1,14 @@
-﻿namespace Com.Application.Defs;
+﻿namespace AirEase_AMS.Application.Defs;
 
 public interface IAircraft
 {
-    
+
+    void SetCruisingSpeed();
+    void SetModelName();
+    void SetFuelCapacity();
+
+    int GetCruisingSpeed();
+    string GetModelName();
+    int GetFuelCapacity();
+
 }
