e9f89041c680f3938015a83e5cfdcab17c94eb93
tj <tj.schwartz2000@gmail.com>
2023-04-03 15:04:51 -0400

    Fixes

--- AirEase-AMS/Application/AirEaseMain.cs
+++ AirEase-AMS/Application/AirEaseMain.cs
@@ -15,8 +15,8 @@ namespace AirEase_AMS.Application
             //Temporary example of using an HLib function. Delete whenever
             int num = HLib.GenerateFiveDigitID();
             Console.WriteLine(num);
-            string salt = HLib.GenerateSalt(1024);
-            string hash = HLib.EncryptPassword("password7", salt);
+            string salt = HLib.GenerateSalt(2);
+            string hash = HLib.EncryptPassword("SomePassword88!", salt);
             Console.WriteLine("Password: " + hash + " \nWhere salt is " + salt);
             Console.WriteLine("Thank you for playing Wing Commander");
         }
