12b349997a7b6c940443ad2cad17f7c5af675985
Kiyle Winborne <kiyleawinborne@gmail.com>
2023-04-04 09:32:14 -0400

    Fixed branch problems with local repo.

---
+++ AirEase-AMS/App/AirEaseMain.cs
@@ -0,0 +1,24 @@
+using AirEase_AMS.Interface;
+using AirEase_AMS.Application;
+
+namespace AirEase_AMS.Application
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            //Temporary example of using an HLib function. Delete whenever
+            int num = HLib.GenerateFiveDigitID();
+            Console.WriteLine(num);
+            string salt = HLib.GenerateSalt(2);
+            string hash = HLib.EncryptPassword("SomePassword88!", salt);
+            Console.WriteLine("Password: " + hash + " \nWhere salt is " + salt);
+            Console.WriteLine("Thank you for playing Wing Commander");
+        }
+    }
+}
