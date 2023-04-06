5c80dca9588445b798944900798bcf988726cf1d
tj <tj.schwartz2000@gmail.com>
2023-04-06 19:34:58 -0400

    Added comments and fixes to the DAO - it works as intended on my localhost. If we ever get it hosted, we just need to change the server to the correct IP address instead of localhost:1433.

--- AirEase-AMS/App/DAO.cs
+++ AirEase-AMS/App/DAO.cs
@@ -18,14 +18,28 @@ public class DatabaseAccessObject
         {
             SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
 
-            //builder.DataSource = "THE";
-            builder["Server"] = "localhost";
-            builder.UserID = "TheBackdoor";
-            builder.Password = "Password123";
+            //This is the name of the SQL Server - don't ask why
+            builder.DataSource = "THE";
+
+            //We need to tell the connection object - WE WANT TO CONNECT USING TCP!
+            //Otherwise, we would need to be a trusted_connection, and talk via pipes.
+            //This works fine if we're only ever using localhost on your local windows device - not optimal!!
+            builder["Server"] = "tcp:localhost";
+
+            //We've spent millions on achieving the best security money can buy
+            builder.UserID = "SecretEntrance";
+            builder.Password = "123456";
+
+            //Our Database catalog
             builder.InitialCatalog = "AirEase";
+
+            //We could encrypt it - but then we would need to implement some certification method... but... lets call that a stretch goal, kay?
             builder.Encrypt = false;
-            builder["Trusted_Connection"] = true;
+
+            //Establish a connection (possible point of failure)
             connection = new SqlConnection(builder.ConnectionString);
+
+            //Once the connection is open, we have finished our job here!!
             connection.Open();
 
         }
@@ -35,13 +49,23 @@ public class DatabaseAccessObject
         }
     }
 
+    /// <summary>
+    /// Standard destructor for DAO. Closes the database connection.
+    /// </summary>
     ~DatabaseAccessObject()
     {
         if (connection != null) { connection.Close(); }
     }
 
+
+    /// <summary>
+    /// A general function for database SELECTS. Expects to get some return value, or it returns null.
+    /// </summary>
+    /// <param name="sqlQuery">The query being passed to the database (WARNING: query needs sanitized before getting here!)</param>
+    /// <returns>A data table object, populated with the results of the input search query. Can be null.</returns>
     public DataTable? Retrieve(string sqlQuery)
     {
+        //No connection established - return error
         if (connection == null)
         {
             Console.WriteLine("Connection is null. Abort.");
@@ -51,16 +75,22 @@ public class DatabaseAccessObject
         {
             String sql = sqlQuery;
 
+            //Establish our command and reader - they will be automatically disposed of when out of scope.
             using (SqlCommand command = new SqlCommand(sql, connection))
             {
                 using (SqlDataReader reader = command.ExecuteReader())
                 {
+                    //No return data - return null object
                     if (!reader.HasRows)
                     {
                         return null;
                     }
                     else
                     {
+                        //The reader is intrinsically intertwined with the database - when it goes out of scope, it loses connection.
+                        //For that reason, it can not be returned here. The DataTable makes for a fine substitute.
+
+                        //Load our data table for return.  
                         System.Data.DataTable dt = new System.Data.DataTable();
                         dt.Load(reader);
                         return dt;
@@ -71,13 +101,21 @@ public class DatabaseAccessObject
         }
         catch (SqlException e)
         {
+            //Print exception info
             Console.WriteLine(e.ToString());
         }
+        //Looks like an exception occurred - return null.
         return null;
     }
 
+    /// <summary>
+    ///  A general function for database edits. Returns the number of rows altered.
+    /// </summary>
+    /// <param name="sqlQuery">The query being passed to the database (WARNING: query needs sanitized before getting here!)</param>
+    /// <returns>The number of rows altered by the input query.</returns>
     public int Update(string sqlQuery)
     {
+        //No connection established - return error
         if (connection == null)
         {
             Console.WriteLine("Connection is null. Abort.");
@@ -88,22 +126,29 @@ public class DatabaseAccessObject
 
             String sql = sqlQuery;
 
+            //Establish our command and reader - they will be automatically disposed of when out of scope.
             using (SqlCommand command = new SqlCommand(sql, connection))
             {
                 using (SqlDataReader reader = command.ExecuteReader())
                 {
+                    //Return the number of rows affected by our input query.
                     return reader.RecordsAffected;
                 }
             }
         }
         catch (SqlException e)
         {
+            //Print exception info
             Console.WriteLine(e.ToString());
         }
+        //Looks like an exception occurred - return error code.
         return -1;
     }
 
-
+    /// <summary>
+    /// DEBUG: Prints the attributes of a data table object.
+    /// </summary>
+    /// <param name="dt">The data table being printed.</param>
     public void PrintDataTable(DataTable? dt)
     {
         if (dt != null)
