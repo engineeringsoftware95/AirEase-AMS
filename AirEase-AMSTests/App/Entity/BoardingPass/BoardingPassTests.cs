36152832f9cecd3690ba989f09db06a014b5b040
Nathan Cruickshank <nathandcruickshank@gmail.com>
2023-04-24 14:06:42 -0400

    Adding BoardingPassTests

---
+++ AirEase-AMSTests/App/Entity/BoardingPass/BoardingPassTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using AirEase_AMS.App.Entity.Aircraft;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirEase_AMS.App.Entity.Aircraft.Tests
+{
+    [TestFixture()]
+    public class BoardingPassTests
+    {
+        [Test()]
+        [TestCase()]
+
+        public void BoardingPassTest(string aircraftName, int numberOfSeats)
+        {
+            Assert.Fail();
+        }
+    }
+}
